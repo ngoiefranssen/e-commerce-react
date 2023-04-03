dfc9401b28851f28d1db6cdc0fdebfc60d7e33ca
ngoiefranssen <ngoiefranssen04@gmail.com>
2023-04-03 21:57:25 +0200

    Wip

--- src/styles/theme/index.js
+++ src/styles/theme/index.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import { createTheme } from "@mui/material/styles";
 
-function index() {
-    return (
-        <div>index</div>
-    )
-}
+export const Colors = {
+    primary: "#5f2c3e",
+    secondary: "#d1adcc",
+    success: "#4CAF50",
+    info: "#00a2ff",
+    danger: "#FF5722",
+    warning: "#FFC107",
+    dark: "#0e1b20",
+    light: "#aaa",
+    muted: "#abafb3",
+    border: "#DDDFE1",
+    inverse: "#2F3D4A",
+    shaft: "#333",
 
-export default index
+    dim_grey: "#696969",
+    dove_grey: "#d5d5d5",
+    body_bg: "#f3f6f9",
+    light_gray: "rgb(230, 230, 230)",
+
+    with: "#fff",
+    black: "#000",
+};
+
+const theme = createTheme({
+
+});
+
+export default theme;
