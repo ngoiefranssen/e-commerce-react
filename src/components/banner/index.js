dce47ec40844b1df884032984203d446653d0760
ngoiefranssen <ngoiefranssen04@gmail.com>
2023-06-18 10:39:03 -0400

    Wip

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -15,11 +15,15 @@ const Banner = () => {
   const Matches = useMediaQuery(theme.breakpoints.down('md'));
   return (
     <BannerContanair>
-      <BannerImage src="/images/name_dossier/name_image.extension" />
+      <BannerImage src="/images/bannier/name_image.extension" />
       <BannerContent>
         <Typography variant="h6">FN collection</Typography>
         <BannerTitle variant="h2">New Bags</BannerTitle>
-        <BannerDescription variant="subtitle">Exemple to shop</BannerDescription>
+        <BannerDescription variant="subtitle">
+          Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut
+          labore et dolore magna
+        </BannerDescription>
+        <BannerShopButton color="primary">Shop Now</BannerShopButton>
       </BannerContent>
     </BannerContanair>
   );
